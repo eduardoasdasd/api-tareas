2895fe84e317eccff2fb87242dd132f43dbf3a15
Eduardo Vasquez <evasquezibanez@gmail.com>
2021-04-11 21:17:21 -0400

    configuracion base proyecto

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express, { json } from 'express';
+import http from 'http';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import '@babel/polyfill';
+//import { sequelize } from './database/database';
+
+const app = express();
+//middlewares
+app.use(json())
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    res.status(200).json({message:"Bienvenido a la primera api"}); 
+})
+
+const server = http.createServer(app);
+
+server.listen('3001', ()=>{
+    console.log('servidor iniciado en el puerto 3001');
+})
